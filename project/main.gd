7af0c86dde425ba8e882606de597812ea5de8a7c
Mangmasta <mangmasta@gmail.com>
2022-09-04 00:28:29 -0700

    Changed build system to cmake

--- project/main.gd
+++ project/main.gd
@@ -5,4 +5,6 @@ const GDDL = preload("res://gdnative/gddl.gdns")
 onready var data = GDDL.new()
 
 func _on_Button_pressed():
-	data.download_file("https://crouton.net", "/tmp/crouton.html")
+	var dl_path = OS.get_data_dir() + '/crouton.html'
+	print_debug("Downloading to:", dl_path)
+	data.download_file("https://crouton.net", dl_path)
