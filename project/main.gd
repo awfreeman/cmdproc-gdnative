ce6ca5a7c0a4e733063844cee81ab4f0752d20fb
Mangmasta <mangmasta@gmail.com>
2022-09-04 02:19:04 -0700

    Changed min version and example code.

--- project/main.gd
+++ project/main.gd
@@ -5,9 +5,9 @@ const GDDL = preload("res://gdnative/gddl.gdns")
 onready var data = GDDL.new()
 
 func _on_Button_pressed():
-	#var dl_path = OS.get_data_dir() + '/crouton.html'
+	var dl_path = OS.get_data_dir() + '/crouton.html'
 	# following should fail, this is used to test error catching
-	var dl_path = '/crouton.html'
+	#var dl_path = '/crouton.html'
 	print_debug("Downloading to:", dl_path)
 	if not data.download_file("https://crouton.net", dl_path):
 		print_debug(data.get_error())
